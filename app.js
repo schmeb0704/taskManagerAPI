3862e68b3a67e41fcffdde7b1b18ceb9c0aa84ef
SZ-Agent <seanauditor0704@gmail.com>
2023-03-15 07:11:02 -0500

    set up mongo database, installed dependencies

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+require("dotenv").config()
 const express = require("express")
 const tasks = require("./routes/tasks")
 const app = express()
 const port = 3000
-
+const connectDB = require("./db/connect")
 
 app.use(express.static("./public"))
 app.use(express.json())
 
 app.use("/api/v1/tasks", tasks)
 
+const start =  async () => { // makes sure that we get connected to the database first before starting the server
+    try{
+        await connectDB(process.env.MONGO_URI)
+        console.log("connected to DB");
+        app.listen(port, ()=>{
+            console.log(`Server is listening on port ${port}...`);
+        })
+    }catch(err){
+        console.log(err);
+    }
+}
 
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}...`);
-})
+start()
+
