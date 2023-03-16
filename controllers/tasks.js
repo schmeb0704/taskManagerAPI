48ac787bedb17cd08dd86078c6eb8acf3328fe1d
SZ-Agent <seanauditor0704@gmail.com>
2023-03-16 05:54:24 -0500

    configured the controllers so that we can do CRUD operations on the database. Also fixed up the Task schema to have some basic validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,47 +1,74 @@
 const Task = require("../models/Task")
 
-const getAllTasks = (req, res) => {
-    res.status(200).json({tasks: []})
+const getAllTasks = async (req, res) => {
+    const tasks = await Task.find({})
+    res.status(200).send({ tasks})
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
+    try {
+        const task = await Task.create(req.body)
+        return res.status(201).json({ task })
+    } catch (err) {
+        console.log(err.errors.name.properties.message);
+        const { message } = err.errors.name.properties
 
-    if(task){
-        return res.status(201).json({task})
-    } else {
-        return res.status(401).json({success: false, msg: "Please enter a task"})
+        if (message.includes("provide tasks")) {
+            return res.status(401).json({ success: false, msg: "Please enter a task" })
+        } else if (message.includes("characters")) {
+            return res.status(401).json({ success: false, msg: "Task name cannot be more than 20 characters" })
+
+        } else{
+            return res.status(500).json({ success: false, msg: "Internal server error", error: err.errors })
+        }
     }
 }
 
-const getTask = (req, res) =>{
-    const {id: taskID} = req.params
-    const task = {name: "feed dog", id: "1w2e3r4t"}
-    if(taskID){
-        return res.status(200).json({task})
-    } else{
-        return res.status(401).json({success: false, msg: "Please enter a task"})
+const getTask = async (req, res) => {
+    const { id: taskID } = req.params
+
+    try{
+        const task = await Task.findOne({_id: taskID})
+        if(!task){
+            return res.status(404).json({success: false, msg: `No task with ID: ${taskID}`})
+        }
+        return res.status(200).json({ task })
+
+    }catch(err){
+        return res.status(500).json({success: false, error: err})
     }
 }
 
-const updateTask = (req, res)=>{
-    const {id: taskID} = req.params
-    
-    if(taskID){
-        return res.status(200).json({success: true, msg: "task updated"})
-    } else{
-        return res.status(401).json({success: false, msg: "Please enter a task"})
+const updateTask = async (req, res) => {
+    const { id: taskID } = req.params
+    const {name, completed} = req.body
+
+    try{
+        const updatedTask = await Task.findByIdAndUpdate(taskID, {name, completed}, {new: true, runValidators: true}) // await Task.findOneAndUpdate({_id: taskID})
+        // const updatedTask = await Task.findOne({_id: taskID})
+        return res.status(200).json({ task: updatedTask })
+    }catch(err){
+        return res.status(500).json({success: false, error: err})
+
+    }
+
+}
+
+const deleteTask = async (req, res) => {
+    const { id: taskID } = req.params
+
+    try{
+        const toDelete = await Task.findByIdAndDelete(taskID) // await Task.findOneAndDelete({_id: taskID})
+
+        if(!toDelete){
+            return res.status(404).json({success: false, msg: `No task with ID: ${taskID}`})
+        }
+
+        res.json({success: true, msg: "task deleted", task: toDelete})
+    }catch(err){
+        console.log(err);
+        res.status(500).json({success: false, error: err})
     }
 }
 
-const deleteTask = (req, res)=>{
-    const {id: taskID} = req.params
-    
-    if(taskID){
-        return res.status(200).json({success: true, msg: "task deleted"})
-    } else{
-        return res.status(401).json({success: false, msg: "Please enter a task"})
-    }
-}
-
-module.exports = {getAllTasks, createTask, getTask, updateTask, deleteTask}
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose')
 
 // create structure/model for data to be sent to database
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    completed:Boolean
-}) 
+    name: {
+        type: String,
+        required: [true, "Must provide task name"],
+        trim: true,
+        maxLength: [20, "Task name cannot be more than 20 characters"]
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
 
 module.exports = mongoose.model("Task", TaskSchema) // mongoose.model(name_of_model, schema)
